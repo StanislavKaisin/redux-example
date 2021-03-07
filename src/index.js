24d6c4f4e0fdc17a6c17f22911d3014650e62781
StanislavKaisin <8050361@gmail.com>
2021-03-07 22:15:26 +0200

    added webpack and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import "./styles.css";
+
+const counter = document.getElementById("counter");
+const addBtn = document.getElementById("add");
+const removeBtn = document.getElementById("remove");
+const asyncBtn = document.getElementById("async");
+const themeBtn = document.getElementById("theme");
+
+let state = 0;
+
+function render() {
+  counter.textContent = state;
+}
+
+addBtn.addEventListener("click", () => {
+  state++;
+  render();
+  // console.log("state=", state);
+});
+
+removeBtn.addEventListener("click", () => {
+  state--;
+  render();
+  // console.log("state=", state);
+});
+
+asyncBtn.addEventListener("click", () => {
+  setTimeout(() => {
+    state++;
+    render();
+  }, 2000);
+});
+
+themeBtn.addEventListener("click", () => {
+  // state++;
+  // render();
+  // console.log("state=", state);
+});
+
+render();
+
+// console.log("counter=", counter);
